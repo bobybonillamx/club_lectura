9cdff864f42d0c70068be5ea31683e352a7f5eb6
Boby Bonilla <support@novarum.mx>
2026-04-23 16:10:38 -0600

    verificador

--- check_theme.py
+++ check_theme.py
@@ -1,7 +1,8 @@
 from core.models import ClubSettings
 c = ClubSettings.get_solo()
-print("tema:", c.theme)
+print("tema en DB:", c.theme)
 print("primary_color:", c.primary_color)
-print("accent_color:", c.accent_color)
-print("custom_font_serif:", c.custom_font_serif)
-print("custom_font_url:", c.custom_font_url[:60] if c.custom_font_url else "")
+tv = c.theme_vars
+print("theme_vars page:", tv.get('page'))
+print("theme_vars primary:", tv.get('primary'))
+print("theme_vars fonts:", tv.get('fonts', '')[:60])
